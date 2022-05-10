35f09b6b8cec38341465c2715d90b0c816d3271e
Rahul <mail@rahul.one>
2022-05-09 22:18:22 -0700

    Unmount and close the disk if a disk has already been mounted on login Add docker-compose.yml

--- mount.sh
+++ mount.sh
@@ -6,6 +6,15 @@
 MAPPER_VOL=disk_vol
 
 printf "$2" | cryptsetup open --type luks $1 $MAPPER_VOL
+
+if [ "$?" -ne "0" ]; then
+    printf "Volume already mounted, unmounting\n"
+    umount /dev/mapper/$MAPPER_VOL
+    cryptsetup luksClose $MAPPER_VOL
+    printf "Filesystem unmounted\n"
+    exit 1
+fi
+
 mkdir -p /srv
 mount /dev/mapper/$MAPPER_VOL /srv
 printf "Filesystem mounted"
